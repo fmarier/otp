6598c004d1079e134de9aa587f38883bbd83d528
Francois Marier <francois@brave.com>
2025-05-16 20:49:15 -0700

    Fix typo in comment

--- totp/totp.go
+++ totp/totp.go
@@ -139,7 +139,7 @@ type GenerateOpts struct {
 	AccountName string
 	// Number of seconds a TOTP hash is valid for. Defaults to 30 seconds.
 	Period uint
-	// Size in size of the generated Secret. Defaults to 20 bytes.
+	// Size in bytes of the generated Secret. Defaults to 20 bytes.
 	SecretSize uint
 	// Secret to store. Defaults to a randomly generated secret of SecretSize.  You should generally leave this empty.
 	Secret []byte
